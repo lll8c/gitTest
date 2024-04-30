67eaa9f62eb7b7ebd648f7ceea68d9ebf2abe7da
lll <977255673@qq.com>
2024-04-30 18:56:28 +0800

    update main.go

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+var (
+	n   int
+	st  [10]bool
+	arr [10]int
+)
+
+func dfs(k int) {
+	if k == n {
+		for i := 0; i < n; i++ {
+			fmt.Println(arr)
+		}
+	}
+
+	for i := 1; i <= n; i++ {
+		if st[i] {
+			continue
+		}
+		st[i] = true
+		arr[k] = i
+		dfs(k + 1)
+		st[i] = false
+	}
+}
+
+func main() {
+	n = 5
+	dfs(0)
 }
